2ddc05f65887c00fff1e21bc740bfdb93ba0f125
James Westfall <james.westfall@neat.no>
2023-04-23 15:07:28 +0200

    Updates

--- features/steps/simple_pretty_formatter.py
+++ features/steps/simple_pretty_formatter.py
@@ -4,25 +4,26 @@ from behave.textutil import indent
 
 class SimplePrettyFormatter(PrettyFormatter):
 
-    def __init__(self, stream_opener, config):
-        config.show_multiline = False
-        super().__init__(stream_opener, config)
+    def match(self, match):
+        self._match = match
+        self.print_statement()
+        self.stream.flush()
+
 
     # -- DISABLE: Step replay.
     def result(self, step):
-        if not self.monochrome:
-            lines = self.step_lines + 1
-            if self.show_multiline:
-                if step.table:
-                    lines += len(step.table.rows) + 1
-                if step.text:
-                    lines += len(step.text.splitlines()) + 2
-            arguments = []
-            location = None
-            if self._match:
-                arguments = self._match.arguments
-                location = self._match.location
-            self.print_step(step.status, arguments, location, True)
+        lines = self.step_lines + 1
+        if self.show_multiline:
+            if step.table:
+                lines += len(step.table.rows) + 1
+            if step.text:
+                lines += len(step.text.splitlines()) + 2
+        arguments = []
+        location = None
+        if self._match:
+            arguments = self._match.arguments
+            location = self._match.location
+        self.print_step(step.status, arguments, location, True)
         if step.error_message:
             self.stream.write(indent(step.error_message.strip(), "      "))
             self.stream.write("\n\n")
